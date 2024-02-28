0fcac26e6bdc1aec9bb4dee9968b42896e4033dc
NakulTalwar10 <tnakult@gmail.com>
2024-02-28 16:50:50 +0530

    Refactor code to fetch posts and categories separately, then restructure the data into a more usable format.

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,12 +1,37 @@
-
-require("dotenv").config()
-const axios= require('axios')
+require("dotenv").config();
+const axios = require('axios');
 
 module.exports.getBlogs = async (req, resp) => {
     try {
-        const response = await axios.get(process.env.URL);
-        const data = response.data;
-        resp.status(200).json(data);
+        const postsResponse = await axios.get(process.env.POSTS_URL);
+        const postsData = postsResponse.data;
+
+        const categoriesResponse = await axios.get(process.env.CATEGORIES_URL);
+        const categoriesData = categoriesResponse.data;
+
+        const categoryMap = {};
+        categoriesData.forEach(category => {
+            categoryMap[category.id] = category.name;
+        });
+
+        const structuredData = {};
+        postsData.forEach(post => {
+            post.categories.forEach(categoryId => {
+                const categoryName = categoryMap[categoryId];
+                if (structuredData.hasOwnProperty(categoryName)) {
+                    structuredData[categoryName].push(post);
+                } else {
+                    structuredData[categoryName] = [post];
+                }
+            });
+        });
+
+        const result = Object.entries(structuredData).map(([categoryName, posts]) => ({
+            categoryname: categoryName,
+            posts: posts
+        }));
+
+        resp.status(200).json(result);
     } catch (err) {
         console.log(err);
         resp.status(500).json({ error: 'Internal Server Error' });
